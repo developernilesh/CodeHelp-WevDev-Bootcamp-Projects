0a7e4c7a319f30d16d1d3c81be58cd3263b95b42
developernilesh <nileshmukherjee2017@gmail.com>
2024-04-12 12:08:42 +0530

    Blogs App

---
+++ React Projects/Blogs App - Context API/src/context/AppCotext.js
@@ -0,0 +1,61 @@
+import { createContext, useState } from "react";
+import { baseUrl } from "../baseUrl";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export function AppContextProvider({children}){
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState('');
+    const navigate = useNavigate();
+
+    async function fetchBlogs(page = 1, tag = null, category = null) {
+        setLoading(true);
+        let url = `${baseUrl}get-blogs?page=${page}`;
+
+        if(tag){
+            url += `&tag=${tag}`
+        }
+        
+        if(category){
+            url += `&category=${category}`
+        }
+
+        try {
+            // console.log(url)
+            const result = await fetch(url);
+            const data = await result.json();
+            // console.log(data)
+
+            setPage(data.page)
+            setPosts(data.posts)
+            setTotalPages(data.totalPages)
+        } 
+        catch (error) {
+            alert("Errror in fetching data");
+            setPage(1)
+            setPosts([]);
+            setTotalPages('')
+        }
+        setLoading(false); 
+    }
+
+    function handlePageChange(page) {
+        navigate({search: `page=${page}`})
+        setPage(page);
+    }
+
+    const value = {
+        posts,setPosts,
+        loading,setLoading,
+        page,setPage,
+        totalPages,setTotalPages,
+        handlePageChange, fetchBlogs
+    }    
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
