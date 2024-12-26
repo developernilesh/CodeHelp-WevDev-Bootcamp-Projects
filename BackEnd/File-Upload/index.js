32c5cffacb170478f774697602b8cc425f74547f
developernilesh <nileshmukherjee2017@gmail.com>
2024-12-26 23:58:43 +0530

    Pushed

--- BackEnd/File-Upload/index.js
+++ BackEnd/File-Upload/index.js
@@ -3,3 +3,20 @@ const app = express();
 
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+const fileupload = require('express-fileupload');
+app.use(fileupload());
+
+const db = require('./config/database')
+db.connect()
+
+const cloudinary = require('./config/cloudinary')
+cloudinary.cloudinaryConnect()
+
+const upload = require('./routes/FileUpload')
+app.use('api/upload',upload)
+
+app.listen(PORT, () => {
+    console.log(`App is running at ${PORT}`);
+})
