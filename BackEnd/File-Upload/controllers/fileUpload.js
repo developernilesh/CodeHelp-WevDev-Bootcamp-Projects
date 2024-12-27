525cb13e34f6dcd18641727c1f550f4df29d1ef1
developernilesh <nileshmukherjee2017@gmail.com>
2024-12-27 23:57:39 +0530

    Pushed

---
+++ BackEnd/File-Upload/controllers/fileUpload.js
@@ -0,0 +1,9 @@
+const File = require('../models/File')
+
+exports.localFileUpload = async(req, res) => {
+    try {
+        const file = req.files.file;
+    } catch (error) {
+        
+    }
+}
--- BackEnd/File-Upload/routes/FileUpload.js
+++ BackEnd/File-Upload/routes/FileUpload.js
@@ -1,2 +1,8 @@
 const express = require('express')
 const router = express.Router();
+
+const {localFileUpload} = require('../controllers/fileUpload')
+
+router.post('/local-file-upload',localFileUpload);
+
+module.exports = router
